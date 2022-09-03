fe945501d502cf28dadaf1ecafebbf3d5a8dad1d
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2022-09-03 22:07:00 +0900

    Terminate after last window closed

--- BoardEditor/BoardEditorApp.swift
+++ BoardEditor/BoardEditorApp.swift
@@ -1,10 +1,19 @@
+import AppKit
 import SwiftUI
 
 @main
 struct BoardEditorApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             SelectView()
         }
+    }
+}
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 }
